0020f8d6fe6155d054c9b86106cf421a6d497307
zhangjieDev-ai <zhangjie4728@gmail.com>
2026-05-17 00:51:20 +0800

    Update 4.0收银机浮点数版.c
    第39行代码，printf("交易失败！钱不够，还差： %.2f 元。请把商品放回去点...\n", total - money);在 %.2f 前面不小心留了一个空格（还差： %.2f 元），这导致控制台打印时，“还差：”和数字之间也会多出一个小空格。虽然不影响代码运行，但以后做极致项目时，把这个空格删掉，排版会更加紧凑。

--- 4.0收银机浮点数版.c
+++ 4.0收银机浮点数版.c
@@ -1,4 +1,4 @@
-//20260201_������4.0_��������//
+//20260201_收银机4.0_浮点数版//
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
@@ -7,11 +7,11 @@ int main()
 	double price = 0;
 	double total = 0;  
 
-	printf("---��ӭʹ���Ž೬�У�4.0�������棩---\n");
+	printf("---欢迎使用张洁超市（4.0浮点数版）---\n");
 
 	while (1)
 	{
-		printf("��������Ʒ�۸�����-1����ɨ�룩��");
+		printf("请输入商品价格（输入-1结束扫码）：");
 		scanf("%lf", &price);
 
 		if (price == -1)
@@ -20,23 +20,23 @@ int main()
 		}
 
 		total = total + price;
-		printf("��ǰ�ۼƽ�%.2f Ԫ\n", total);
+		printf("当前累计金额：%.2f 元\n", total);
 	}
 
 	printf("\n==================================\n");
-	printf("������Ʒ�ܼƣ�%.2f Ԫ\n", total);
+	printf("所有商品总计：%.2f 元\n", total);
 
 	double money = 0;
-	printf("��������֧���Ľ�");
+	printf("请输入您支付的金额：");
 	scanf("%lf", &money);
 
 	if (money >= total)
 	{
-		printf("���׳ɹ����Ҹ�����%.2f Ԫ����ӭ�´ι��٣��Ž࣡\n", money - total);
+		printf("交易成功！找给您：%.2f 元。欢迎下次光临，张洁！\n", money - total);
 	}
 	else
 	{
-		printf("����ʧ�ܣ�Ǯ��������� %.2f Ԫ�������Ʒ�Ż�ȥ��...\n", total - money);
+		printf("交易失败！钱不够，还差：%.2f 元。请把商品放回去点...\n", total - money);
 	}
 
 	return  0;
